11f183a0d9961e0bb168e6ff2a2b4e14b4ff393e
Lawrence O. Caulker <locaulker@gmail.com>
2019-06-01 18:33:16 -0400

    initial commit w/template setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>A Travel Site</title>
+  <!-- uikit2 css -->
+  <link rel="stylesheet" href="/uikit/css/uikit.min.css">
+  <!-- <link rel="stylesheet" href="/uikit/css/style.css"> -->
+  <!-- google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700i&display=swap" rel="stylesheet">
+  <!-- custom css -->
+  <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+  <h1>Travel Site</h1>
+  <a href="#" class="uk-button uk-button-primary">DEMO</a>
+
+
+
+  <!-- jQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- uikit2 js -->
+  <script src="/uikit/js/uikit.min.js"></script>
+</body>
+</html>
